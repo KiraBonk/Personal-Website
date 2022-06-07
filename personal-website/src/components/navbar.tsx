25fb33ed76acede4a693b1ecd41dbb2a5c6e2ec3
KiraBonk <videostuff16@gmail.com>
2022-06-07 18:08:23 -0300

    Updated NavBar
    Added link interaction with page links, dark/light mode working. (Doesn't have the right colours yet, but hey it works)

--- personal-website/src/components/navbar.tsx
+++ personal-website/src/components/navbar.tsx
@@ -6,37 +6,67 @@ import {
   Heading,
   HStack,
   Link,
+  useColorMode,
+  useColorModeValue,
+  Text,
 } from "@chakra-ui/react";
-import { MoonIcon } from "@chakra-ui/icons";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import NextLink from "next/link";
 
 const Navbar = () => {
+  const { toggleColorMode } = useColorMode();
+  const bgColour = useColorModeValue("#ADD8E6", "#7997A1");
+  const iconSymbol = useColorModeValue(<MoonIcon />, <SunIcon />);
+  const iconColorScheme = useColorModeValue("pink", "yellow");
   return (
-    <Box position="fixed" as="nav" w="full" bg="#202023">
+    <Box position="fixed" as="nav" w="full" bg={bgColour}>
       <Container display="flex" p={2} maxW="container.md">
         <Container>
           <Flex alignItems="center" mr={2}>
-            <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+            <Heading
+              _hover={{
+                textDecoration: "underline",
+                textDecorationThickness: "8%",
+              }}
+              as="h1"
+              size="lg"
+              letterSpacing={"tighter"}
+            >
               <Link as={NextLink} href="/">
                 Noah Aalders
               </Link>
             </Heading>
           </Flex>
           <HStack alignItems="center" pl={5}>
-            <Link as={NextLink} href="/projects">
-              Projects
-            </Link>
-            <Link as={NextLink} href="/about-me">
-              About Me
-            </Link>
+            <Text
+              _hover={{
+                textDecoration: "underline",
+                textDecorationThickness: "8%",
+              }}
+            >
+              <Link as={NextLink} href="/projects">
+                Projects
+              </Link>
+            </Text>
+            <Text
+              _hover={{
+                textDecoration: "underline",
+                textDecorationThickness: "8%",
+              }}
+            >
+              <Link as={NextLink} href="/about-me">
+                About Me
+              </Link>
+            </Text>
           </HStack>
         </Container>
         <Container>
           <Box flex={1} align="right" pt={3}>
             <IconButton
+              onClick={toggleColorMode}
               aria-label="Change Theme"
-              icon={<MoonIcon />}
-              colorScheme="purple"
+              icon={iconSymbol}
+              colorScheme={iconColorScheme}
               size="sm"
             />
           </Box>
